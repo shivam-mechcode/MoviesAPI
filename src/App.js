a5d474754093cc922b28e1d3f4ae441eb41852ac
shivam-mechcode <70307820+shivam-mechcode@users.noreply.github.com>
2022-09-07 20:29:20 +0530

    API calls

--- src/App.js
+++ src/App.js
@@ -5,75 +5,114 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
 
 
-  const fetchMovieHandler = () =>{
-    fetch('https://swapi.dev/api/films/').then(response => {
-      console.log(response)
-      return response.json()
-    }).then((data) =>{
+  const  fetchMovieHandler=async()=>{
+    setLoading(true)
+    setError(null)
+    try{
+      const response = await fetch('https://swapi.dev/api/films/')
+      if(!response.ok){
+       throw new Error('Something is wrong')
+      }
+    console.log(response)
+     const data = await response.json()
+    
       const transformedMovies = data.results.map(movieData => {
-        return{
-          ...movieData
-        }
+        return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_date,
+          };
       })
       console.log(data.results)
 
       setMovies(transformedMovies)
-    })
+    } 
+    catch(error){
+      console.log(error)
+      setError(false) 
+    }
+    setLoading(false)
   }
-
-  return (
+    return (
     <React.Fragment>
       <section>
+        
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
+        
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length===0 && !error && <p>Found no Movies</p>}
+        {!isLoading && error && <p>{error}</p>} 
+        {isLoading && <p>Loading...</p>}
+        
       </section>
     </React.Fragment>
   );
-}
+  }
 
 export default App;
+
 
 // import React, { useState } from 'react';
 
 // import MoviesList from './components/MoviesList';
 // import './App.css';
-
 // function App() {
 //   const [movies, setMovies] = useState([]);
+//   const [isLoading, setLoading] = useState(false)
+//   const [error, setError] = useState(null)
 
-//   function fetchMoviesHandler() {
-//     fetch('https://swapi.dev/api/films/')
-//       .then((response) => {
-//         return response.json();
-//       })
-//       .then((data) => {
-//         console.log(data.results)
-//         const transformedMovies = data.results.map((movieData) => {
-//           return {
+
+//   const  fetchMovieHandler=async()=>{
+//     setLoading(true)
+//     setError(null)
+//     try{
+//       const response = await fetch('https://swapi.dev/api/film/')
+//       if(!response.ok){
+//        throw new Error('Something is wrong')
+//       }
+//     console.log(response)
+//      const data = await response.json()
+    
+//       const transformedMovies = data.results.map(movieData => {
+//         return {
 //             id: movieData.episode_id,
 //             title: movieData.title,
 //             openingText: movieData.opening_crawl,
 //             releaseDate: movieData.release_date,
 //           };
-//         });
-//         setMovies(transformedMovies);
-//       });
-//   }
+//       })
+//       console.log(data.results)
 
-//   return (
+//       setMovies(transformedMovies)
+//     } 
+//     catch(error){
+//       console.log(error)
+//       setError(false) 
+//     }
+//     setLoading(false)
+//   }
+//     return (
 //     <React.Fragment>
 //       <section>
-//         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        
+//         <button onClick={fetchMovieHandler}>Fetch Movies</button>
+        
 //       </section>
 //       <section>
-//         <MoviesList movies={movies} />
+//         {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+//         {!isLoading && movies.length===0 && !error && <p>Found no Movies</p>}
+//         {!isLoading && error && <p>{error}</p>} 
+//         {isLoading && <p>Loading...</p>}
+        
 //       </section>
 //     </React.Fragment>
 //   );
-// }
-
+//   }
 // export default App;
